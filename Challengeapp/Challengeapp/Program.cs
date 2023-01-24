a6741de138108157b6eb239f2a418c51a1328425
Piotr Rogalewski <piotr.rogalewski@gmail.com>
2023-01-25 00:10:06 +0100

    To tak dla zabawy

--- Challengeapp/Challengeapp/Program.cs
+++ Challengeapp/Challengeapp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Piotr";
+var age = 34;
+bool isActive = true;
+if (name != "Piotr")
+{
+    isActive = false;
+}
+Console.WriteLine("CZY NAZYWASZ SIĘ PIOTR? I CZY MASZ PONIŻEJ 40 LAT?");
+if (age < 40)
+
+{
+    if (isActive == true)
+    {
+        Console.WriteLine("- Tak. Jestem Piotr, mam poniżej 40 lat.");
+    }
+    else
+    {
+
+        Console.WriteLine("- Jestem kimś innym, ale mam poniżej 40 lat.");
+    }
+}
+else if (age < 50)
+{
+    Console.WriteLine("- Mam pomiędzy 40, a 50 lat!");
+    if (isActive == true)
+    {
+        Console.WriteLine("I nazywam się Piotr.");
+    }
+    else
+    {
+        Console.WriteLine("Jestem kimś innym.");
+    }
+}
+else
+{
+    Console.WriteLine("- Jestem starszy...");
+    if (isActive == true)
+    {
+        Console.WriteLine("...Ale nazywam się Piotr.");
+    }
+    else 
+    {
+        Console.WriteLine("...i nie nazywam się Piotr.");
+    }
+}
+
