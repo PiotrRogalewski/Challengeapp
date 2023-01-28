3f301882abf03b9bcbbd208e69626a50dca3b6cc
Piotr Rogalewski <piotr.rogalewski@gmail.com>
2023-01-28 01:48:23 +0100

    Zadanie domowe - dzień 6

---
+++ Challengeapp/Challengeapp/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace Challengeapp
+
+{
+    public class Employee
+    {
+        public static string NameOfScoreboard = "           EMPLOYEE SCOREBOARD";
+
+        private List<int> employeeScore = new List<int>();
+
+        public Employee(string name, string surname, string age)
+        {
+            this.Name = name;
+            this.Surname = " " + surname + ",";
+            this.Age = " lat " + age;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Age { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return this.employeeScore.Sum();
+            }
+        }
+
+        public void AddScore(int points)
+        {
+            this.employeeScore.Add(points);
+        }
+
+    }
+}
--- Challengeapp/Challengeapp/Program.cs
+++ Challengeapp/Challengeapp/Program.cs
@@ -1,16 +1,60 @@
-﻿Console.Write("Wpisz liczbę i naciśnij ENTER: ");
-string chooseYourNumber = Console.ReadLine();
-char[] digits = chooseYourNumber.ToCharArray();
+﻿using Challengeapp;
 
-for (int n = 0; n < 10; n++)
+var boardTitle = Employee.NameOfScoreboard;
+
+Console.WriteLine(boardTitle);
+Console.WriteLine(" ");
+Console.WriteLine("     Points:      Employee details: ");
+Console.WriteLine(" ");
+
+Employee employee1 = new Employee("Jan", "Kowalski", "34");
+Employee employee2 = new Employee("Anna", "Nowak", "31");
+Employee employee3 = new Employee("Leon", "Wiśniewski", "38");
+
+employee1.AddScore(4);
+employee1.AddScore(8);
+employee1.AddScore(7);
+employee1.AddScore(5);
+employee1.AddScore(8);
+
+employee2.AddScore(5);
+employee2.AddScore(6);
+employee2.AddScore(5);
+employee2.AddScore(9);
+employee2.AddScore(7);
+
+employee3.AddScore(3);
+employee3.AddScore(5);
+employee3.AddScore(9);
+employee3.AddScore(8);
+employee3.AddScore(9);
+
+int result1 = employee1.Result;
+int result2 = employee2.Result;
+int result3 = employee3.Result;
+
+var person1 = employee1.Name + employee1.Surname + employee1.Age;
+var person2 = employee2.Name + employee2.Surname + employee2.Age;
+var person3 = employee3.Name + employee3.Surname + employee3.Age;
+
+List<Employee> employeeList = new List<Employee>()
 {
-    int count = 0;
-    foreach (char digit in digits)
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employeeList)
+{
+    if (employee.Result > maxResult)
     {
-        if (n == char.GetNumericValue(digit))
-        {
-            count++;
-        }
+        employeeWithMaxResult = employee;
     }
-    Console.WriteLine(n + " => " + count);
 }
+
+Console.WriteLine("       " + result1 + "    -->  " + person1);
+Console.WriteLine("       " + result2 + "    -->  " + person2);
+Console.WriteLine("       " + result3 + "    -->  " + person3);
+Console.WriteLine(" ");
+Console.WriteLine(" Employee of the week is: " + employeeWithMaxResult.Name + employeeWithMaxResult.Surname + employeeWithMaxResult.Age + ", this person achieved the highest score with the number of points: " + employeeWithMaxResult.Result + ".");
